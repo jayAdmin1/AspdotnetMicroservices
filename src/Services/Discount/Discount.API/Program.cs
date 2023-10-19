7195a5a130ac14506d9169aebee2af5facbe16e8
Jay Dalia <thescalion104@gmail.com>
2023-10-19 11:56:16 +0530

    Commit for completion of Discount API

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,4 +1,5 @@
-using Basket.API.Repositories;
+using Discount.API.Extensions;
+using Discount.API.Repositories;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,25 +9,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
 });
-
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
-});
-builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 var app = builder.Build();
 
+app.MigrateDatabase<Program>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
 }
 
 app.UseAuthorization();
