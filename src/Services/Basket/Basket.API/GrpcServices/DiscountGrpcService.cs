0c0dd1eadf50b00a1a7df61f61ab251f18c9c244
Jay Dalia <thescalion104@gmail.com>
2023-10-20 15:21:31 +0530

    Commit for completion of Discount.API and Discount.Grpc Api

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,20 @@
+﻿using Discount.Grpc.Protos;
+using System.Runtime.CompilerServices;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService;
+        }
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest {ProducName = productName };
+            return await _discountProtoService.GetDiscountAsync(discountRequest);
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,6 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +18,10 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o => o.Address = new Uri(builder.Configuration.GetValue<string>("GrpcSettings:DiscountUrl")));
+
+builder.Services.AddScoped<DiscountGrpcService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
