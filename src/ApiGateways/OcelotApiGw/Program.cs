3434d0b312673f5740669acf023aa84df69b2de8
Jay Dalia <thescalion104@gmail.com>
2023-11-07 09:58:30 +0530

    Commit for Shopping Aggregator project

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,33 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
+builder.WebHost.ConfigureLogging((hostingContext, loggingbuilder) =>
+{
+    loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingbuilder.AddConsole();
+    loggingbuilder.AddDebug();
+});
+
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+var app = builder.Build();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Hello World!");
+    });
+});
+//app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
